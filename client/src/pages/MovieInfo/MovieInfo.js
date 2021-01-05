6a50e892b5f232bbd7f233de7bc75b91749e3048
ikevinws <kevinwang1036@gmail.com>
2021-01-05 18:36:43 -0500

    added modals for movie info page

--- client/src/pages/MovieInfo/MovieInfo.js
+++ client/src/pages/MovieInfo/MovieInfo.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import { Button, Alert, Row, Col, Image } from 'react-bootstrap';
 import './MovieInfo.scss';
 //context
@@ -12,12 +12,16 @@ import { getMovieImage } from '../../utils/Movie';
 import ScrollTop from '../../components/ScrollTop/ScrollTop';
 import Loader from '../../components/Loader/Loader';
 import MovieRating from '../../components/MovieRating/MovieRating';
+import Modal from '../../components/Modal/Modal';
 
 const MovieInfo = () => {
     const { isAdmin } = useAuthContext();
     let { id: movieId } = useParams();
+    const history = useHistory();
+    //states
     const [hasError, setHasError] = useState(false);
     const [hasAuthError, setHasAuthError] = useState(false);
+    const [showDeleteModal, setShowDeleteModal] = useState(false);
     const [movie, setMovie] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
 
@@ -44,16 +48,21 @@ const MovieInfo = () => {
 
     const handleDeleteMovie = async () => {
         if (isAdmin) {
-            try {
-                const res = await deleteMovieById(movieId);
-                if (res.status === 204) {
-                    //modal
-                    console.log('movie deleted');
-                }
-            } catch {
-                setHasAuthError(true);
-            }
+            setHasAuthError(false);
+            setShowDeleteModal(true);
         } else {
+            setShowDeleteModal(false);
+            setHasAuthError(true);
+        }
+    };
+
+    const deleteMovie = async () => {
+        try {
+            const res = await deleteMovieById(movieId);
+            if (res.status === 204) {
+                history.push('/');
+            }
+        } catch {
             setHasAuthError(true);
         }
     };
@@ -61,16 +70,25 @@ const MovieInfo = () => {
     return (
         <ScrollTop>
             <div className="p-5">
-                {hasAuthError ? (
-                    <Alert variant="info" onClose={() => setHasAuthError(false)} dismissible>
-                        Only admins can edit or delete movies.
-                    </Alert>
-                ) : null}
                 {hasError ? (
                     <Alert variant="danger" onClose={() => setHasError(false)} dismissible>
                         Error: Movie could not be loaded. Please try again at a later time.
                     </Alert>
                 ) : null}
+                <Modal
+                    show={hasAuthError}
+                    header={'Error'}
+                    message={'Only admins can edit or delete movies.'}
+                    onHide={() => setHasAuthError(false)}
+                />
+                <Modal
+                    show={showDeleteModal}
+                    header={'Delete Confirmation'}
+                    message={'Are you sure you want to delete this movie?'}
+                    onHide={() => setShowDeleteModal(false)}
+                    onConfirm={() => deleteMovie()}
+                />
+
                 {isLoading ? (
                     <Loader />
                 ) : movie ? (
