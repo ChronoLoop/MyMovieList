4936b553fe3018bf80981913770153b70496df84
ikevinws <kevinwang1036@gmail.com>
2022-09-02 18:24:44 -0400

    rollback passport to v4

--- server/config/database.js
+++ server/config/database.js
@@ -4,7 +4,7 @@ const logMsg = (msg) => {
 
 const dbConfig = async (mongoose, DATABASE_URL) => {
     try {
-        mongoose.connect(DATABASE_URL);
+        await mongoose.connect(DATABASE_URL);
         const dbConnection = mongoose.connection;
         // handle errors after initial connection was established
         dbConnection.on('error', (err) => logMsg(err));
